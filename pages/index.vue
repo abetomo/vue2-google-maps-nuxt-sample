2c6f159148ec77d11110a3b6a0686194945ec040
abetomo <abe@enzou.tokyo>
2018-08-03 18:13:06 +0900

    Incorporate GoogleMap

--- pages/index.vue
+++ pages/index.vue
@@ -19,15 +19,62 @@
           class="button--grey">GitHub</a>
       </div>
     </div>
+    <div>
+      <gmap-map
+        :center=center
+        :zoom=zoom
+        style="width: 500px; height: 500px">
+        <gmap-marker
+          :key="index"
+          v-for="(m, index) in markers"
+          :position="m.position"
+          :clickable="true"
+          :draggable="true"
+          @click="markerClick(m.position)" />
+      </gmap-map>
+    </div>
   </section>
 </template>
 
 <script>
 import AppLogo from '~/components/AppLogo.vue'
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: API_KEY,
+    libraries: 'places,drawing,visualization',
+    language: 'ja',
+    region: 'JP'
+  }
+})
 
 export default {
   components: {
     AppLogo
+  },
+  data () {
+    return {
+      center: {
+        lat: 35.6588126,
+        lng: 139.6985006
+      },
+      zoom: 15,
+      markers: [
+        {
+          position: {
+            lat: 35.6588126,
+            lng: 139.6985006
+          }
+        }
+      ]
+    }
+  },
+  methods: {
+    markerClick: function (position) {
+      console.log(JSON.stringify(position))
+    }
   }
 }
 </script>
