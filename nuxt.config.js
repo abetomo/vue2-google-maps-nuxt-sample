eac6deb25a24ce7d580159c59fe9f883b9bef8f0
abetomo <abe@enzou.tokyo>
2018-08-03 16:47:48 +0900

    Add vue2-google-maps setting to nuxt.config.js
    https://github.com/xkjyeah/vue-google-maps#nuxtjs-config

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,17 @@ module.exports = {
     ** Run ESLint on save
     */
     extend (config, { isDev, isClient }) {
+      if (!isClient) {
+        // This instructs Webpack to include `vue2-google-maps`'s Vue files
+        // for server-side rendering
+        config.externals.splice(0, 0, function (context, request, callback) {
+          if (/^vue2-google-maps($|\/)/.test(request)) {
+            callback(null, false)
+          } else {
+            callback()
+          }
+        })
+      }
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
